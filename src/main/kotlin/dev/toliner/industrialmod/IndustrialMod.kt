838007f5affdc6994b06c6c8307e42ef71866b71
kobi32768 <kobi32768@gmail.com>
2020-08-01 23:02:51 +0900

    Add itemGroup

--- src/main/kotlin/dev/toliner/industrialmod/IndustrialMod.kt
+++ src/main/kotlin/dev/toliner/industrialmod/IndustrialMod.kt
@@ -1,16 +1,29 @@
 package dev.toliner.industrialmod
 
+import net.minecraft.item.ItemGroup
+import net.minecraft.item.ItemStack
+import net.minecraft.item.Items
 import net.toliner.korgelin.api.KotlinMod
 import net.toliner.korgelin.container.KotlinModLoadingContext
 import java.util.logging.LogManager
+import javax.annotation.Nonnull
 
-@KotlinMod("industrialmod")
+
+@KotlinMod(IndustrialMod.MODID)
 object IndustrialMod {
     private val logger = LogManager.getLogManager()!!
+
+    const val MODID: String = "industrialmod"    // rename "mystechrious"
+    const val MODNAME: String = "Industrial Mod" // rename "Mystechrious"
 
     init {
         KotlinModLoadingContext.get().eventBus.apply {
 
         }
+    }
+
+    val itemGroup: ItemGroup = object : ItemGroup(MODID) {
+        @Nonnull
+        override fun createIcon(): ItemStack { return ItemStack(Items.DIAMOND) }
     }
 }
